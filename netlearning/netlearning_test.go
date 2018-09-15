99412a33138dbfcfecc1540896697a035127f8a6
Jiawei <xujavacn@gmail.com>
2018-09-15 16:28:19 +0800

    add address lookup

--- netlearning/netlearning_test.go
+++ netlearning/netlearning_test.go
@@ -1,6 +1,7 @@
 package netlearning_test
 
 import (
+	// "fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "net"
@@ -14,5 +15,19 @@ var _ = Describe("Netlearning", func() {
 
 	It("check IPv4 len", func() {
 		Expect(IPv4len).To(Equal(4))
+	})
+
+	It("check IPv6 len", func() {
+		Expect(IPv6len).To(Equal(16))
+	})
+
+	It("join host and port", func() {
+		result := JoinHostPort("github.com", "443")
+		Expect(result).To(Equal("github.com:443"))
+	})
+
+	It("look up addr", func() {
+		names, _ := LookupAddr("192.30.253.112")
+		Expect(len(names)).NotTo(Equal(0))
 	})
 })
